41faa68fdf7ffd5c45a58645ec3b382ba6860ad0
TreleaniFederico <treleani.federico@gmail.com>
2022-08-23 16:38:59 +0200

    add html layout add js users input

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// users data input
+const distance = parseInt(prompt('Quanti km vuoi percorrere?'));
+
+const age = parseInt(prompt('Quanti anni hai?'));
+
+const priceKm = 0.21;
+
+let ticketPrice = prompt(priceKm * distance);
