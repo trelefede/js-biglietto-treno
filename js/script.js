1f6898713458570e17160ff65e214af53e39c462
TreleaniFederico <treleani.federico@gmail.com>
2022-08-23 17:10:54 +0200

    add input if isNaN

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-// users data input
-const distance = parseInt(prompt('Quanti km vuoi percorrere?'));
-
-const age = parseInt(prompt('Quanti anni hai?'));
-
 const priceKm = 0.21;
 
-let ticketPrice = prompt(priceKm * distance);
+// users data input
+const distance = parseInt(prompt('Quanti km vuoi percorrere?'));
+if (isNaN(distance)) {
+    alert('Valore inserito non valido!');
+} else {
+    const age = parseInt(prompt('Quanti anni hai?'));
+    if (isNaN(age)) {
+        alert('Valore inserito non valido!');
+    } else {
+        // price calc 
+        let ticketPrice = priceKm * distance;
+        // alert travel price
+        alert('Il prezzo del viaggio è ' + ticketPrice + "€")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
